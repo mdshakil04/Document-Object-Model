a7a1242a6fff351581ca8b8068217a9f02e3fe88
mdshakil04 <m.shakil04@yahoo.com>
2024-02-21 11:55:26 +0600

    New Practices added

--- js/dom.js
+++ js/dom.js
@@ -5,6 +5,10 @@ for(const li of liCollection){
 }
 const allHeading = document.getElementsByTagName('h1');
 for(const h1 of allHeading){
+    h1.style.textAlign = 'center';
+    h1.style.color= 'red';
+    h1.style.backgroundColor = 'green'
+    h1.style.borderRadius= '10px'
     // console.log(h1.innerText);
 }
 const myDaughter = document.getElementById('my-star').style.backgroundColor = 'skyblue';
@@ -12,12 +16,16 @@ const myDaughter = document.getElementById('my-star').style.backgroundColor = 's
 myDaughter.innerText = 'Miftahul';
 const mainHeader = document.getElementById('header');
 // mainHeader.innerText = "Only DOM is real";
-mainHeader.innerHTML = "<h1>This is my Awesome DOM</h1>"
+mainHeader.innerHTML = "<h1>This is my Awesome JS DOM</h1>"
+// mainHeader.title = "DOM"
 
 const mains = document.querySelectorAll('main');
 // console.log(mains)
 for(const main of mains){
     // console.log(main)
     main.style.border = ' 2px solid steelblue';
-    main.style.padding = '10px'
+    main.style.padding = '10px';
+    main.style.backgroundColor = 'skyblue';
+    main.style.borderRadius = '10px';
+    // main.style.textAlign = 'center';
 }

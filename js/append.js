de0a688a5fcc5050db1013a13afaf79a4de76ff8
mdshakil04 <m.shakil04@yahoo.com>
2024-02-21 12:51:53 +0600

    New section created with JavaScript

--- js/append.js
+++ js/append.js
@@ -5,6 +5,19 @@ const membersList = document.getElementById('members-list')
 // What will add
 const newMember = document.createElement('li');
 // set added text
-newMember.innerText = 'Sister-in-law';
+newMember.innerText = 'বেজাইল্লা Sister-in-law';
 // Bind with parent
 membersList.appendChild(newMember)
+
+const memberContainer = document.getElementById('family-container')
+const newContainer = document.createElement('section')
+memberContainer.appendChild(newContainer)
+newContainer.innerHTML = `
+    <h1>My Permanent Family</h1>
+    <ul>
+        <li>Maa</li>
+        <li>Baba</li>
+        <li>Bai</li>
+        <li>Bon</li>
+    </ul>
+`
